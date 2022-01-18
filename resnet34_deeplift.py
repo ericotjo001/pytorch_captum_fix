8591fe19c400865ba8e24b2460f7f1eaa3b8c074
ababa003 <97959587+ababa003@users.noreply.github.com>
2022-01-18 23:59:06 +0800

    Update resnet34_deeplift.py

--- resnet34_deeplift.py
+++ resnet34_deeplift.py
@@ -79,6 +79,7 @@ class Resnet34c(nn.Module):
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     net = Resnet34c().to(device=device)
+    net.eval()
 
     """
     pil_img: PIL image of a dog and a cat
